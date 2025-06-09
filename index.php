c78f30fc319fe6845b27e00911735131c2423bbe
Albert Nabarrete <ahnabarrete@gmail.com>
2025-06-09 17:06:24 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+
+
+    // VARIAVEIS DO SEO
+    $title_pagina           = "Buffet";
+
+    // DEPOIMENTOS
+     $depoimentos = array(
+        array(
+            "depoimento" => "Fiz parte do curso de Formação Para Cidadania no período de vinte e dias, e aprendi conceitos muito importantes a respeito do mercado de trabalho, tanto com atividades práticas quanto teóricas.
+                                A equipe de funcionários é muito competente e o serviço fornecido também foi ótimo."
+        ),
+        array(
+            "depoimento" => "Minha experiência na formação para cidadania foi ótima, tenho a agradecer pelo conhecimento adquirido que acredito que vai me auxiliar muito no mercado de trabalho e em outras áreas também."
+        ),
+        array(
+            "depoimento" => "Gostei muito da experiência do Camp Pinheiros, me proporcionou a aprender muitas coisas novas, principalmente minha comunicação que era informal, mais com a Mayara e a Érica consegui desenvolver melhor minha comunicação, estou grato ao Camp Pinheiros e as orientadoras, super recomendo a todos"
+        ),
+        array(
+            "depoimento" => "Eu gostei muito das experiências que tive aqui no camp. Aprendi muita coisa e já estou colocando em prática o que eu aprendi"
+        )
+    );
+    
+    $doencas = array(
+        array(
+            "doenca" => "Insônia"
+        ),
+        array(
+            "doenca" => "Ansiedade"
+        ),
+        array(
+            "doenca" => "Dores crônicas"
+        ),
+        array(
+            "doenca" => "Epilepsia"
+        ),
+        array(
+            "doenca" => "Alzheimer"
+        ),
+        array(
+            "doenca" => "Autismo"
+        ),
+        array(
+            "doenca" => "Câncer"
+        ),
+        array(
+            "doenca" => "Glaucoma"
+        ),
+        array(
+            "doenca" => "Parkinson"
+        ),
+    
+    );
+
+    // PERGUNTAS FREQUENTES
+    $perguntas_frequrntes = array(
+        array(
+            "pergunta" => "Quem pode consumir canabidiol?",
+            "resposta" => "Lorem ipsum risus nam duis imperdiet consequat viverra, aliquam nostra ut lacus aliquet consectetur, porta vitae fringilla euismod tempus lacus."
+        ),
+        array(
+            "pergunta" => "Como faço para poder tratar com a cannabis medicinal?",
+            "resposta" => "Lorem ipsum risus nam duis imperdiet consequat viverra, aliquam nostra ut lacus aliquet consectetur, porta vitae fringilla euismod tempus lacus."
+        ),
+        array(
+            "pergunta" => "O que é o CannaID?",
+            "resposta" => "Lorem ipsum risus nam duis imperdiet consequat viverra, aliquam nostra ut lacus aliquet consectetur, porta vitae fringilla euismod tempus lacus."
+        ),
+        array(
+            "pergunta" => "Como funciona o tratamento?",
+            "resposta" => "Lorem ipsum risus nam duis imperdiet consequat viverra, aliquam nostra ut lacus aliquet consectetur, porta vitae fringilla euismod tempus lacus."
+        ),
+        array(
+            "pergunta" => "Uso prolongado faz mal?",
+            "resposta" => "Lorem ipsum risus nam duis imperdiet consequat viverra, aliquam nostra ut lacus aliquet consectetur, porta vitae fringilla euismod tempus lacus."
+        ),
+        array(
+            "pergunta" => "Vicia?",
+            "resposta" => "Lorem ipsum risus nam duis imperdiet consequat viverra, aliquam nostra ut lacus aliquet consectetur, porta vitae fringilla euismod tempus lacus."
+        ),
+
+    );
+
+    include("./includes/head.php");
+    
+?>
+
+<?php
+    include("./includes/footer.php");
+?>
